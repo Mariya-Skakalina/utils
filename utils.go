3f9226ed57e111d3914d1fc1707b7a6de22ae692
Maria <m.skakalina@yandex.ru>
2022-06-17 00:36:16 +0300

    Tag 1

--- utils.go
+++ utils.go
@@ -1,19 +1,25 @@
 package utils
 
+import "fmt"
+
 func Contains(a []string, x string) bool {
 	for _, n := range a {
 		if x == n {
+			fmt.Print("True")
 			return true
 		}
 	}
+	fmt.Print("False")
 	return false
 }
 
 func ContainsInt(a []int, x int) bool {
 	for _, n := range a {
 		if x == n {
+			fmt.Print("True")
 			return true
 		}
 	}
+	fmt.Print("False")
 	return false
 }
